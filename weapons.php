91685dcc6862dfbcfca8e4b7227222d1e8355a0d
John Myles <jmyles@jmyles.co.uk>
2014-06-15 22:24:25 +0100

    Updated weapons script and removed old split spell section scripts

--- weapons.php
+++ weapons.php
@@ -10,52 +10,24 @@ $melee_header = <<<END
 		<div class="sheet-row sheet-sub-header">
 			<div class="sheet-col-1-24 sheet-center sheet-small-label sheet-vert-bottom">Prof?</div>
 			<div class="sheet-col-1-6 sheet-center sheet-small-label sheet-vert-bottom">Weapon</div>
-			<div class="sheet-col-1-12 sheet-center sheet-small-label sheet-vert-bottom">Type</div>
 			<div class="sheet-col-1-8 sheet-center sheet-small-label sheet-vert-bottom">Weilded</div>
-			<div class="sheet-col-1-8 sheet-center sheet-small-label sheet-vert-bottom">Properties</div>
+			<div class="sheet-col-1-12 sheet-center sheet-small-label sheet-vert-bottom">Finesse?</div>
 			<div class="sheet-col-1-12 sheet-center sheet-small-label sheet-vert-bottom">Magic Bonus</div>
+			<div class="sheet-col-1-24 sheet-center sheet-small-label sheet-vert-bottom">&nbsp;</div>
 			<div class="sheet-col-1-24 sheet-center sheet-small-label sheet-vert-bottom">To<br/>Hit</div>
+			<div class="sheet-col-1-12 sheet-center sheet-small-label sheet-vert-bottom">Attack</div>
 			<div class="sheet-col-1-12 sheet-center sheet-small-label sheet-vert-bottom">Base Damage</div>
 			<div class="sheet-col-1-24 sheet-center sheet-small-label sheet-vert-bottom">Dmg<br/>Bonus</div>
 			<div class="sheet-col-1-8 sheet-center sheet-small-label sheet-vert-bottom">Damage Type</div>
-			<div class="sheet-col-1-12 sheet-center sheet-small-label sheet-vert-bottom">Macro</div>
+			<div class="sheet-col-1-12 sheet-center sheet-small-label sheet-vert-bottom">Damage</div>
 		</div>
-END;
-
-$ranged_header = <<<END
-		<h4 class="sheet-center">Ranged Weapons <span class="sheet-pictos">e</span></h4>
-		<div class="sheet-row sheet-sub-header">
-			<div class="sheet-col-1-24 sheet-center sheet-small-label sheet-vert-bottom">Prof?</div>
-			<div class="sheet-col-1-12 sheet-center sheet-small-label sheet-vert-bottom">Ammo</div>
-			<div class="sheet-col-1-6 sheet-center sheet-small-label sheet-vert-bottom">Weapon</div>
-			<div class="sheet-col-1-8 sheet-center sheet-small-label sheet-vert-bottom">Type</div>
-			<div class="sheet-col-1-24 sheet-center sheet-small-label sheet-vert-bottom">To<br/>Hit</div>
-			<div class="sheet-col-1-6 sheet-center sheet-small-label sheet-vert-bottom">Properties</div>
-			<div class="sheet-col-1-12 sheet-center sheet-small-label sheet-vert-bottom">Base Damage</div>
-			<div class="sheet-col-1-8 sheet-center sheet-small-label sheet-vert-bottom">Damage Type</div>
-			<div class="sheet-col-1-12 sheet-center sheet-small-label sheet-vert-bottom">Magic Bonus</div>
-			<div class="sheet-col-1-12 sheet-center sheet-small-label sheet-vert-bottom">Macro</div>
-		</div>
-				
-END;
-
-
-$wrapper_end = <<<END
-		<hr/>
 END;
 
 $melee_rows = <<<'END'
-	
 	<!-- BEGIN melee weapon row -->
 	<div class="sheet-row">
 		<div class="sheet-col-1-24 sheet-checkbox-row"><input type="checkbox" value="@{PB}" name="attr_pbmeleeCURRENTROW" checked="checked"></div>
 		<div class="sheet-col-1-6"><input type="text" name="attr_meleeweaponnameCURRENTROW"></div>
-		<div class="sheet-col-1-12">
-			<select name="attr_meleeattackstatCURRENTROW">
-				<option value="@{strength_mod}">Normal</option>
-				<option value="@{finesse_mod}">Finesse</option>
-			</select>
-		</div>
 		<div class="sheet-col-1-8" title="How is the weapon being wielded">
 			<select name="attr_meleeattackweildedCURRENTROW">
 				<option value="1">Main Hand</option>
@@ -64,16 +36,42 @@ $melee_rows = <<<'END'
 				<option value="1">Off Hand (2weap fight style)</option>
 			</select>
 		</div>
-		<div class="sheet-col-1-8"><input type="text" name="attr_meleepropertiesCURRENTROW"></div>
+		<div class="sheet-col-1-12">
+			<select name="attr_meleeattackstatCURRENTROW">
+				<option value="@{strength_mod}">No</option>
+				<option value="@{finesse_mod}">Yes</option>
+			</select>
+		</div>
 		<div class="sheet-col-1-12" title="The magic bonus will be added as a bonus to BOTH the attack and damage rolls"><input type="number" name="attr_meleemagicCURRENTROW" value="0"></div>
+		<div class="sheet-col-1-24">&nbsp;</div>
 		<div class="sheet-col-1-24"><input type="number" name="attr_meleetohitCURRENTROW" value="@{meleeattackstatCURRENTROW} + @{pbmeleeCURRENTROW} + @{meleemagicCURRENTROW}" disabled="disabled"></div>
+		<div class="sheet-col-1-12 sheet-center"><button type="roll" class="sheet-roll" name="roll_MeleeAttackCURRENTROW" value="/em uses @{meleeweaponnameCURRENTROW} to attack\n\n[[1d20 + @{meleetohitCURRENTROW} [To Hit] ]] vs AC" >Attack</button></div>
 		<div class="sheet-col-1-12" title="Only enter the base damage roll here without any bonuses from stats or other sources"><input class="sheet-center" type="text" name="attr_meleedmgCURRENTROW"></div>
 		<div class="sheet-col-1-24"><input type="number" name="attr_meleedmgbonusCURRENTROW" value="(@{meleeattackstatCURRENTROW} * @{meleeattackweildedCURRENTROW}) + @{meleemagicCURRENTROW}" disabled="disabled"></div>
 		<div class="sheet-col-1-8"><input type="text" name="attr_meleedmgtypeCURRENTROW"></div>
-		<div class="sheet-col-1-12 sheet-center"><button type="roll" class="sheet-roll" name="roll_MeleeAttackCURRENTROW" value="/em uses @{meleeweaponnameCURRENTROW} to attack\n\n[[1d20 + @{meleetohitCURRENTROW} [To Hit] ]] vs AC\n\n[[@{meleedmgCURRENTROW} [Base damage] + (@{meleeattackstatCURRENTROW} * @{meleeattackweildedCURRENTROW}) [Damage Stat Mod] + @{meleemagicCURRENTROW} [Magic Bonus] ]] @{meleedmgtypeCURRENTROW} damage on a hit" >Attack</button></div>
+		<div class="sheet-col-1-12 sheet-center"><button type="roll" class="sheet-roll" name="roll_MeleeDamageCURRENTROW" value="For [[@{meleedmgCURRENTROW} [Base damage] + @{meleedmgbonusCURRENTROW} [Damage Bonus] ]] @{meleedmgtypeCURRENTROW} damage" >Damage</button></div>
 	</div>
 	<!-- END melee weapon row -->
 
+END;
+
+$ranged_header = <<<END
+		<h4 class="sheet-center">Ranged Weapons <span class="sheet-pictos">e</span></h4>
+		<div class="sheet-row sheet-sub-header">
+			<div class="sheet-col-1-24 sheet-center sheet-small-label sheet-vert-bottom">Prof?</div>
+			<div class="sheet-col-1-12 sheet-center sheet-small-label sheet-vert-bottom">Ammo</div>
+			<div class="sheet-col-1-6 sheet-center sheet-small-label sheet-vert-bottom">Weapon (range)</div>
+			<div class="sheet-col-1-8 sheet-center sheet-small-label sheet-vert-bottom">Type</div>
+			<div class="sheet-col-1-12 sheet-center sheet-small-label sheet-vert-bottom">Magic Bonus</div>
+			<div class="sheet-col-1-24 sheet-center sheet-small-label sheet-vert-bottom">&nbsp;</div>
+			<div class="sheet-col-1-24 sheet-center sheet-small-label sheet-vert-bottom">To<br/>Hit</div>
+			<div class="sheet-col-1-12 sheet-center sheet-small-label sheet-vert-bottom">Attack</div>
+			<div class="sheet-col-1-12 sheet-center sheet-small-label sheet-vert-bottom">Base Damage</div>
+			<div class="sheet-col-1-24 sheet-center sheet-small-label sheet-vert-bottom">Dmg<br/>Bonus</div>
+			<div class="sheet-col-1-8 sheet-center sheet-small-label sheet-vert-bottom">Damage Type</div>
+			<div class="sheet-col-1-12 sheet-center sheet-small-label sheet-vert-bottom">Damage</div>
+		</div>
+				
 END;
 
 $ranged_rows = <<<'END'
@@ -88,15 +86,22 @@ $ranged_rows = <<<'END'
 				<option value="@{strength_mod}">Thrown</option>
 			</select>
 		</div>
-		<div class="sheet-col-1-24"><input type="number" name="attr_rangedtohitCURRENTROW" value="@{rangedtypeCURRENTROW} + @{pbrangedCURRENTROW} + @{rangedmagicCURRENTROW}" disabled="disabled"></div>
-		<div class="sheet-col-1-6"><input type="text" name="attr_rangedpropertiesCURRENTROW"></div>
-		<div class="sheet-col-1-12" title="Only enter the base damage roll here without any bonuses from stats or other sources"><input type="text" name="attr_rangeddmgCURRENTROW"></div>
-		<div class="sheet-col-1-8"><input type="text" name="attr_rangeddmgtypeCURRENTROW"></div>
 		<div class="sheet-col-1-12" title="The magic bonus will be added as a bonus to BOTH the attack and damage rolls"><input type="number" name="attr_rangedmagicCURRENTROW" value="0"></div>
-		<div class="sheet-col-1-12 sheet-center"><button type="roll" class="sheet-roll" name="roll_RangedAttackCURRENTROW" value="/em uses @{rangedweaponnameCURRENTROW} to attack\n\n[[1d20 + @{rangedtohitCURRENTROW} [To Hit] ]] vs AC\n\n[[@{rangeddmgCURRENTROW} [Base damage] + @{rangedtypeCURRENTROW} [Damage Stat Mod] + @{rangedmagicCURRENTROW} [Magic Bonus] ]] @{rangeddmgtypeCURRENTROW} damage on a hit" >Attack</button></div>
+		<div class="sheet-col-1-24">&nbsp;</div>
+		<div class="sheet-col-1-24"><input type="number" name="attr_rangedtohitCURRENTROW" value="@{rangedtypeCURRENTROW} + @{pbrangedCURRENTROW} + @{rangedmagicCURRENTROW}" disabled="disabled"></div>
+		<div class="sheet-col-1-12 sheet-center"><button type="roll" class="sheet-roll" name="roll_RangedAttackCURRENTROW" value="/em uses @{rangedweaponnameCURRENTROW} to attack\n\n[[1d20 + @{rangedtohitCURRENTROW} [To Hit] ]] vs AC" >Attack</button></div>
+		<div class="sheet-col-1-12" title="Only enter the base damage roll here without any bonuses from stats or other sources"><input class="sheet-center" type="text" name="attr_rangeddmgCURRENTROW"></div>		
+		<div class="sheet-col-1-24"><input type="number" name="attr_rangeddmgbonusCURRENTROW" value="@{rangedtypeCURRENTROW} + @{rangedmagicCURRENTROW}" disabled="disabled"></div>
+		<div class="sheet-col-1-8"><input type="text" name="attr_rangeddmgtypeCURRENTROW"></div>
+		<div class="sheet-col-1-12 sheet-center"><button type="roll" class="sheet-roll" name="roll_RangedDamageCURRENTROW" value="For [[@{rangeddmgCURRENTROW} [Base damage] + @{rangeddmgbonusCURRENTROW} [Damage Bonus] ]] @{rangeddmgtypeCURRENTROW} damage" >Damage</button></div>
 	</div>
 	<!-- END ranged weapon row -->
 
+END;
+
+
+$wrapper_end = <<<END
+		<hr/>
 END;
 
 $full_output = "";
