54e155a575134836e18d8510590976f3f7a3f7cd
John Myles <jmyles@jmyles.co.uk>
2014-06-23 02:20:11 +0100

    Updated spells script to accommodate spellbook rework

--- spells.php
+++ spells.php
@@ -50,11 +50,11 @@ $spell_row = <<<'END'
 				</div>
 				
 				<div class="sheet-row">
-					<div class="sheet-col-1-12 sheet-vert-middle sheet-center">RAWLEVEL<input type="hidden" name="attr_spellbaselevel" value="PAGENUMBER"></div>	
+					<div class="sheet-col-1-12 sheet-vert-middle sheet-center">FRIENDLYLEVEL<input type="hidden" name="attr_spellbaselevel" value="PAGENUMBER"><input type="hidden" name="attr_spellfriendlylevel" value="FRIENDLYLEVEL"></div>	
 					<div class="sheet-col-1-3 sheet-vert-middle"><input type="text" class=" sheet-center" name="attr_spellname"></div>
 					<div class="sheet-col-1-8 sheet-vert-middle">
 						<select name="attr_spellschool">
-							<option value="n/a" selected="selected">n/a</option>
+							<option value="" selected="selected">n/a</option>
 							<option value="Abjuration">Abjuration</option>
 							<option value="Conjuration">Conjuration</option>
 							<option value="Divination">Divination</option>
@@ -66,8 +66,18 @@ $spell_row = <<<'END'
 						</select>
 					</div>
 					<div class="sheet-col-1-6 sheet-vert-middle"><input type="text" class="sheet-center" name="attr_spellcasttime"></div>
-					<div class="sheet-col-1-12 sheet-checkbox-row"><input type="checkbox" value="(Concentration)" name="attr_spellconcentration"></div>
-					<div class="sheet-col-1-12 sheet-checkbox-row"><input type="checkbox" value="(Ritual)" name="attr_spellritual"></div>
+					<div class="sheet-col-1-12 sheet-checkbox-row">
+						<select name="attr_spellconcentration">
+							<option value="" selected="selected">No</option>
+							<option value="(Concentration)">Yes</option>
+						</select>
+					</div>
+					<div class="sheet-col-1-12 sheet-checkbox-row">
+						<select name="attr_spellritual">
+							<option value="" selected="selected">No</option>
+							<option value="(Ritual)">Yes</option>
+						</select>
+					</div>
 					<div class="sheet-col-1-8 sheet-vert-middle" title="Always prepared means the spell is either a cantrip or one that was provided to you via a method which indicated it would never count against your prepared limit for the day">
 						<select name="attr_spellisprepared">
 							<option value="1">Yes</option>
@@ -88,41 +98,56 @@ $spell_row = <<<'END'
 				<div class="sheet-row">
 					<div class="sheet-col-1-8 sheet-vert-middle" title="Use this field to indicate where you learned or have access to this spell from.  Useful to know if multiclassing or if you gain access to spells your class would not normally have thanks to subclass bonuses">
 						<select name="attr_spellgainedfrom">
-							<option value="0">None</option>
-							<option value="@{bard_spell_dc}">Bard</option>
-							<option value="@{cleric_spell_dc}">Cleric</option>
-							<option value="@{druid_spell_dc}">Druid</option>
-							<option value="@{mage_spell_dc}">Mage</option>
-							<option value="@{paladin_spell_dc}">Paladin</option>
-							<option value="@{ranger_spell_dc}">Ranger</option>
-							<option value="0">Other</option>
+							<option value="Not Set">Not Set</option>
+							<option value="Bard">Bard</option>
+							<option value="Cleric">Cleric</option>
+							<option value="Druid">Druid</option>
+							<option value="Mage">Mage</option>
+							<option value="Paladin">Paladin</option>
+							<option value="Ranger">Ranger</option>
+							<option value="Other Source">Other</option>
 						</select>
 					</div>	
-					<div class="sheet-col-1-3 sheet-vert-middle"><input type="text" class="sheet-center" name="attr_spelltarget" value="1 creature"></div>
+					<div class="sheet-col-1-3 sheet-vert-middle"><input type="text" class="sheet-center" name="attr_spelltarget"></div>
 					<div class="sheet-col-1-8 sheet-vert-middle"><input type="text" class="sheet-center" name="attr_spellrange"></div>
 					<div class="sheet-col-1-8 sheet-vert-middle"><input type="text" class="sheet-center" name="attr_spellduration"></div>
 					<div class="sheet-col-7-24 sheet-vert-middle"><input type="text" class="sheet-center" name="attr_spellcomponents"></div>
 				</div>
 				
 				<span class="sheet-small-label">Show :</span>
-				<input type="checkbox" name="attr_spelltypeattack" class="sheet-spelltypetab sheet-spelltypeattack" value="1" title="Attack Roll"/>
-				<input type="checkbox" name="attr_spelltypesave" class="sheet-spelltypetab sheet-spelltypesave" value="1" title="Saving Throw"/>
-				<input type="checkbox" name="attr_spelltypeheal" class="sheet-spelltypetab sheet-spelltypeheal" value="1" title="Healing"/>
-				<input type="checkbox" name="attr_spelltypedescription" class="sheet-spelltypetab sheet-spelltypedesc" value="1" title="Desc."/>
+				<input type="checkbox" name="attr_spelltypeadvanced" class="sheet-spelltypetab sheet-spelltypeadvanced" value="1" title="Advanced"/> |
+				<input type="checkbox" name="attr_spelltypeattack" class="sheet-spelltypetab sheet-spelltypeattack" value="1" title="Attack"/>
+				<input type="checkbox" name="attr_spelltypesave" class="sheet-spelltypetab sheet-spelltypesave" value="1" title="Save"/>
+				<input type="checkbox" name="attr_spelltypeheal" class="sheet-spelltypetab sheet-spelltypeheal" value="1" title="Healing"/> |
+				<input type="checkbox" name="attr_spelltypdamage" class="sheet-spelltypetab sheet-spelltypedamage" value="1" title="Damage"/>
+				<input type="checkbox" name="attr_spelltypeeffects" class="sheet-spelltypetab sheet-spelltypeeffects" value="1" title="Effects"/>
+				
+				<div class="sheet-spell-type-advanced">
+					<div class="sheet-row">
+						<div class="sheet-col-1-2 sheet-vert-bottom sheet-center sheet-small-label">Spell Description/Flavour</div>
+						<div class="sheet-col-1-3 sheet-vert-bottom sheet-center sheet-small-label">Higher Spell Slot Effect</div>
+						<div class="sheet-col-1-6 sheet-vert-bottom sheet-center sheet-small-label">&nbsp;</div>
+					</div>
+					
+					<div class="sheet-row">
+						<div class="sheet-col-1-2 sheet-small-label sheet-center"><textarea class="sheet-medium-textarea" name="attr_spelldescription"></textarea></div>
+						<div class="sheet-col-1-3 sheet-small-label sheet-center"><textarea name="attr_spellhighersloteffect" class="sheet-medium-textarea"></textarea></div>
+						<div class="sheet-col-1-6 sheet-center"><button type="roll" class="sheet-roll" name="roll_SpellInfo" value="/em looks at the details for @{spellname}\n\nName : @{spellname}\n@{spellschool} @{spellfriendlylevel}\nCast time : @{spellcasttime}\nRange : @{spellrange}\nDuration : @{spellduration}\n\n@{spellconcentration} @{spellritual}\n\nDescription : \n@{spelldescription}\n\n\nAt Higher Levels :\n@{spellhighersloteffect}">Spell Card</button></div>
+					</div>
+				
+				
+				</div>
 		
 				<div class="sheet-spell-type-attack">
+				
 					<div class="sheet-row">
-						<div class="sheet-col-1-12 sheet-vert-bottom sheet-center sheet-small-label">Attack Stat</div>
-						<div class="sheet-col-1-6 sheet-vert-bottom sheet-center sheet-small-label">Damage</div>
-						<div class="sheet-col-1-8 sheet-vert-bottom sheet-center sheet-small-label">Damage Type</div>
-						<div class="sheet-col-7-24 sheet-vert-bottom sheet-center sheet-small-label">Additional Effects</div>
-						<div class="sheet-col-1-4 sheet-vert-bottom sheet-center sheet-small-label">Higher Spell Slot Effect</div>
-						<div class="sheet-col-1-12 sheet-vert-bottom sheet-center sheet-small-label">Macro</div>
+						<div class="sheet-col-1-6 sheet-offset-1-3 sheet-vert-bottom sheet-center sheet-small-label">Attack Stat</div>
+						<div class="sheet-col-1-6 sheet-offset-1-3 sheet-vert-bottom sheet-center sheet-small-label">&nbsp;</div>
 					</div>
 							
 					<div class="sheet-row">
-						<div class="sheet-col-1-12 sheet-small-label sheet-center">
-							<select name="attr_attackspellattackstat">
+						<div class="sheet-col-1-6 sheet-offset-1-3  sheet-small-label sheet-center">
+							<select name="attr_attackstat">
 								<option value="0">None</option>
 								<option value="@{strength_mod}">STR</option>
 								<option value="@{dexterity_mod}">DEX</option>
@@ -132,11 +157,7 @@ $spell_row = <<<'END'
 								<option value="@{charisma_mod}">CHA</option>
 							</select>
 						</div>
-						<div class="sheet-col-1-6 sheet-small-label sheet-center"><input type="text" name="attr_attackspelldamage"></div>
-						<div class="sheet-col-1-8 sheet-small-label sheet-center"><input type="text" name="attr_attackspelldamagetype"></div>
-						<div class="sheet-col-7-24 sheet-small-label sheet-center"><input type="text" name="attr_attackspelldamageeffects" value="None"></div>
-						<div class="sheet-col-1-4 sheet-small-label sheet-center"><input type="text" name="attr_attackspellhighersloteffect" value="None"></div>
-						<div class="sheet-col-1-12 sheet-vert-middle sheet-center"><button type="roll" class="sheet-roll" name="roll_AttackSpell" value="/em uses @{spellname} to attack \n\n[[1d20 + @{attackspellattackstat} [Attack stat mod] + @{PB} [Proficiency Bonus] + @{global_spell_attack_bonus} [Active Spell Attack Bonus] ]] | [[1d20 + @{attackspellattackstat} [Attack stat mod] + @{PB} [Proficiency Bonus] + @{global_spell_attack_bonus} [Active Spell Attack Bonus] ]] vs AC\n\n[[@{attackspelldamage} [Base Spell Damage] + @{global_spell_damage_bonus} [Active Spell Damage Bonus] + 0d0 [Bugfix 0]]] @{attackspelldamagetype} damage on a hit\n\nAdditional effect on a hit : @{attackspelldamageeffects}">Use</button></div>
+						<div class="sheet-col-1-6 sheet-offset-1-3 sheet-vert-middle sheet-center"><button type="roll" class="sheet-roll" name="roll_Attack" value="/em uses @{spellname} to attack \n\n[[1d20 + @{attackstat} [Attack stat mod] + @{PB} [Proficiency Bonus] + @{global_spell_attack_bonus} [Active Spell Attack Bonus] ]] | [[1d20 + @{attackstat} [Attack stat mod] + @{PB} [Proficiency Bonus] + @{global_spell_attack_bonus} [Active Spell Attack Bonus] ]] vs AC">Attack</button></div>
 					</div>
 				</div>
 		
@@ -145,16 +166,13 @@ $spell_row = <<<'END'
 						<div class="sheet-col-1-12 sheet-center sheet-small-label">Saving Stat</div>
 						<div class="sheet-col-1-8 sheet-center sheet-small-label">Save DC</div>
 						<div class="sheet-col-1-12 sheet-center sheet-small-label">Custom DC</div>
-						<div class="sheet-col-1-12 sheet-center sheet-small-label">Damage</div>
-						<div class="sheet-col-1-8 sheet-center sheet-small-label">Damage Type</div>
-						<div class="sheet-col-1-4 sheet-center sheet-small-label">Other effects</div>
-						<div class="sheet-col-1-6 sheet-center sheet-small-label">Successful Save</div>
-						<div class="sheet-col-1-12 sheet-center sheet-small-label">Macro</div>
+						<div class="sheet-col-13-24 sheet-center sheet-small-label">On a successful save</div>
+						<div class="sheet-col-1-6 sheet-center sheet-small-label">&nbsp;</div>
 					</div>
 					
 					<div class="sheet-row">
 						<div class="sheet-col-1-12">
-							<select name="attr_savespellsavestat">
+							<select name="attr_savestat">
 								<option value="STR">STR</option>
 								<option value="DEX">DEX</option>
 								<option value="CON">CON</option>
@@ -164,7 +182,7 @@ $spell_row = <<<'END'
 							</select>
 						</div>
 						<div class="sheet-col-1-8" title="Pick the class that the save DC will be created from or set your own DC by selecting custom here and then entering the DC in the next field">
-							<select name="attr_savespellsavedc">
+							<select name="attr_spellsavedc">
 								<option value="0">Custom DC</option>
 								<option value="@{bard_spell_dc}">Bard DC</option>
 								<option value="@{cleric_spell_dc}">Cleric DC</option>
@@ -174,41 +192,51 @@ $spell_row = <<<'END'
 								<option value="@{ranger_spell_dc}">Ranger DC</option>
 							</select>
 						</div>
-						<div class="sheet-col-1-12"><input type="number" name="attr_savespellcustomdc" value="0" min="0" step="1" title="Unless you have selected Custom in the previous field this should always be 0"></div>
-						<div class="sheet-col-1-12"><input type="text" name="attr_savespellbasedmg"></div>
-						<div class="sheet-col-1-8"><input type="text" name="attr_savespelldmgtype"></div>
-						<div class="sheet-col-1-4"><input type="text" name="attr_savespellsavefail"></div>
-						<div class="sheet-col-1-6"><input type="text" name="attr_savespellsavesuccess"></div>
-						<div class="sheet-col-1-12 sheet-vert-middle sheet-center"><button type="roll" class="sheet-roll" name="roll_SaveThrowSpell" value="/em uses @{spellname} to attack\n\nDC [[@{savespellsavedc} + @{savespellcustomdc}]] @{savespellsavestat} saving throw\nOn a failed save :\n[[@{savespellbasedmg} [Base Damage] + @{global_spell_damage_bonus} [Active Spell Damage Bonus] + 0d0 [Bugfix 0] ]] @{savespelldmgtype} damage and @{savespellsavefail}\nOn a successful save :\n@{savespellsavesuccess}" >Use</button></div>
+						<div class="sheet-col-1-12"><input type="number" name="attr_customsavedc" value="0" min="0" step="1" title="Unless you have selected Custom in the previous field this should always be 0"></div>
+						<div class="sheet-col-13-24"><input type="text" class="sheet-center" name="attr_savesuccess"></div>
+						<div class="sheet-col-1-6 sheet-vert-middle sheet-center"><button type="roll" class="sheet-roll" name="roll_Save" value="/em uses @{spellname} to attack\n\nDC [[@{spellsavedc} + @{customsavedc}]] @{savestat} saving throw\nOn a successful save :\n@{savesuccess}" >Attack</button></div>
 						
 					</div>
 				</div>
-		
+						
 				<div class="sheet-spell-type-heal">
-				
 					<div class="sheet-row">
-						<div class="sheet-col-1-8 sheet-vert-bottom sheet-center sheet-small-label">Heal amount</div>
-						<div class="sheet-col-1-2 sheet-vert-bottom sheet-center sheet-small-label">Additional effects</div>
-						<div class="sheet-col-1-4 sheet-vert-bottom sheet-center sheet-small-label">Higher Spell Slot effect</div>
-						<div class="sheet-col-1-12 sheet-vert-bottom sheet-center sheet-small-label">Macro</div>
+						<div class="sheet-col-1-3 sheet-offset-1-4 sheet-vert-bottom sheet-center sheet-small-label">Heal amount</div>
+						<div class="sheet-col-1-6 sheet-offset-1-4 sheet-vert-bottom sheet-center sheet-small-label">&nbsp;</div>
 					</div>
 							
 					<div class="sheet-row">
-						<div class="sheet-col-1-8 sheet-small-label sheet-center"><input type="text" name="attr_healspellhealamount"></div>
-						<div class="sheet-col-1-2 sheet-small-label sheet-center"><input type="text" name="attr_healspelleffect" value="None"></div>
-						<div class="sheet-col-1-4 sheet-small-label sheet-center"><input type="text" name="attr_healspellhighersloteffect" value="None"></div>
-						<div class="sheet-col-1-12 sheet-vert-middle sheet-center"><button type="roll" class="sheet-roll" name="roll_HealSpell" value="/em uses @{spellname} to heal\n\n[[@{healspellhealamount}]] hp healed\nAdditional effect : @{healspelleffect}">Use</button></div>
+						<div class="sheet-col-1-3 sheet-offset-1-4 sheet-small-label sheet-center"><input type="text" class="sheet-center" name="attr_spellhealamount"></div>
+						<div class="sheet-col-1-6 sheet-offset-1-4 sheet-vert-middle sheet-center"><button type="roll" class="sheet-roll" name="roll_Heal" value="/em uses @{spellname} to heal\n\n[[@{spellhealamount}]] hp healed\nAdditional effects: @{spelleffect}">Heal</button></div>
 					</div>
+				</div>
 				
-				</div>
-		
-				<div class="sheet-spell-type-desc">
+				<div class="sheet-spell-type-damage">
 					<div class="sheet-row">
-						<div class="sheet-col-1-10 sheet-vert-middle sheet-small-label sheet-center sheet-margin-top">Spell Description</div>
-						<div class="sheet-col-9-10"><textarea class="sheet-fluid-textarea" name="attr_spelldescription"></textarea></div>
+						<div class="sheet-col-1-4 sheet-offset-1-4 sheet-vert-bottom sheet-center sheet-small-label">Damage Amount</div>
+						<div class="sheet-col-1-4 sheet-vert-bottom sheet-center sheet-small-label">Damage Type</div>
+						<div class="sheet-col-1-6 sheet-offset-1-12 sheet-vert-bottom sheet-center sheet-small-label">&nbsp;</div>
+					</div>
+							
+					<div class="sheet-row">
+						<div class="sheet-col-1-4 sheet-offset-1-4 sheet-small-label sheet-center sheet-margin-top"><input type="text" class="sheet-center" name="attr_damage"></div>
+						<div class="sheet-col-1-4 sheet-small-label sheet-center sheet-margin-top"><input type="text" class="sheet-center" name="attr_damagetype"></div>
+						<div class="sheet-col-1-6 sheet-offset-1-12 sheet-vert-middle sheet-center"><button type="roll" class="sheet-roll" name="roll_Damage" value="[[@{damage} [Base Spell Damage] + @{global_spell_damage_bonus} [Active Spell Damage Bonus] + 0d0 [Bugfix 0]]] @{damagetype} damage\nAdditional effects: @{spelleffect}">Damage</button></div>
 					</div>
 				</div>
-			
+				
+				<div class="sheet-spell-type-effects">
+					<div class="sheet-row">
+						<div class="sheet-col-5-6 sheet-vert-bottom sheet-center sheet-small-label">Spell Effects</div>
+						<div class="sheet-col-1-6 sheet-vert-bottom sheet-center sheet-small-label">&nbsp;</div>
+					</div>
+							
+					<div class="sheet-row">
+						<div class="sheet-col-5-6 sheet-small-label sheet-center"><textarea name="attr_spelleffect" class="sheet-medium-textarea">None</textarea></div>
+						<div class="sheet-col-1-6 sheet-vert-middle sheet-center"><button type="roll" class="sheet-roll" name="roll_SpellEffect" value="@{spelleffect}">Effect</button></div>
+					</div>
+				</div>
+
 			</div>
 		
 			<!-- END spell row -->	
@@ -225,8 +253,8 @@ for ($i=$start; $i<=$max; $i++)
 	// Reset current loop vars
 	$return_text = "";
 	
-	if ($current_page == 0) $raw_level = "Cantrip";
-	else $raw_level = "Level ". $current_page;
+	if ($current_page == 0) $friendly_level = "Cantrip";
+	else $friendly_level = "Level ". $current_page;
 	
 	// Add start of page wrapper
 	$return_text .= $wrapper_start;
@@ -240,7 +268,7 @@ for ($i=$start; $i<=$max; $i++)
 	// Replace placeholders with correct values
 	$return_text = str_replace("PAGENUMBER", $current_page, $return_text);
 	$return_text = str_replace("SPELLLEVEL", $spell_level_array[$current_page], $return_text);
-	$return_text = str_replace("RAWLEVEL", $raw_level, $return_text);
+	$return_text = str_replace("FRIENDLYLEVEL", $friendly_level, $return_text);
 	$return_text = str_replace("SPELLSHORTLEVEL", strtolower(str_replace(" ", "", $spell_level_array[$current_page])), $return_text);
 		
 	
