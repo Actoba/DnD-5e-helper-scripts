c01da733e3d496a08cab9e8f8c1b8579096b9ba9
John Myles <jmyles@jmyles.co.uk>
2015-03-01 12:52:35 +0000

    Updated melee and ranged weapons layout and added use of new crit range field for both

--- weapons.php
+++ weapons.php
@@ -18,7 +18,7 @@ $melee_header = <<<'END'
 			<div class="sheet-col-1-24 sheet-center sheet-small-label sheet-vert-bottom">Dmg<br/>Bonus</div>
 			<div class="sheet-col-1-8 sheet-center sheet-small-label sheet-vert-bottom sheet-padr">Damage Type</div>
 			<div class="sheet-col-1-12 sheet-center sheet-small-label sheet-vert-bottom">Crit Dmg</div>
-			<div class="sheet-col-1-12 sheet-center sheet-small-label sheet-vert-bottom sheet-padr">Crit Range</div>
+			<div class="sheet-col-1-12 sheet-center sheet-small-label sheet-vert-bottom sheet-padr">Crit on a</div>
 			<div class="sheet-col-1-12 sheet-center sheet-small-label sheet-vert-bottom">Attack</div>
 		</div>
 END;
@@ -42,7 +42,7 @@ $melee_rows = <<<'END'
 		<div class="sheet-col-1-8 sheet-padr"><input type="text" name="attr_meleedmgtypeCURRENTROW"></div>
 		<div class="sheet-col-1-12"><input class="sheet-center" type="text" name="attr_meleecritdmgCURRENTROW"></div>
 		<div class="sheet-col-1-12 sheet-padr"><input type="number" name="attr_meleeweaponcritrangeCURRENTROW" value="20" step="1"></div>
-		<div class="sheet-col-1-12 sheet-center"><button type="roll" class="sheet-roll" name="roll_MeleeAttackCURRENTROW" value="&{template:5eDefault} {{weapon=1}} {{title=@{meleeweaponnameCURRENTROW}}} {{subheader=@{character_name}}} {{subheaderright=Melee attack}} {{subheader2=@{meleeweaponpropsCURRENTROW}}} {{attack=[[1d20 + @{meleetohitCURRENTROW} + (@{global_melee_attack_bonus}) ]]}} {{attackadv=[[1d20 + @{meleetohitCURRENTROW} + (@{global_melee_attack_bonus}) ]]}} {{damage=[[@{meleedmgCURRENTROW} + @{meleedmgbonusCURRENTROW} + (@{global_melee_damage_bonus})  + 0d0]] @{meleedmgtypeCURRENTROW}}} {{critdamage=Additional [[@{meleecritdmgCURRENTROW}]] damage}}" >Attack</button></div>
+		<div class="sheet-col-1-12 sheet-center"><button type="roll" class="sheet-roll" name="roll_MeleeAttackCURRENTROW" value="&{template:5eDefault} {{weapon=1}} {{title=@{meleeweaponnameCURRENTROW}}} {{subheader=@{character_name}}} {{subheaderright=Melee attack}} {{attack=[[1d20cs>@{meleeweaponcritrangeCURRENTROW} + @{meleetohitCURRENTROW} + (@{global_melee_attack_bonus}) ]]}} {{attackadv=[[1d20cs>@{meleeweaponcritrangeCURRENTROW} + @{meleetohitCURRENTROW} + (@{global_melee_attack_bonus}) ]]}} {{damage=[[@{meleedmgCURRENTROW} + @{meleedmgbonusCURRENTROW} + (@{global_melee_damage_bonus})  + 0d0]] @{meleedmgtypeCURRENTROW}}} {{critdamage=Additional [[@{meleecritdmgCURRENTROW}]] damage}}" >Attack</button></div>
 	</div>
 	<!-- END melee weapon row -->
 
@@ -52,16 +52,17 @@ $ranged_header = <<<'END'
 		<h4 class="sheet-center">Ranged Weapons <span class="sheet-pictos">e</span></h4>
 		<div class="sheet-row sheet-sub-header">			
 			<div class="sheet-col-1-24 sheet-center sheet-small-label sheet-vert-bottom">Prof?</div>
-			<div class="sheet-col-1-20 sheet-center sheet-small-label sheet-vert-bottom">Ammo</div>
-			<div class="sheet-col-1-8 sheet-center sheet-small-label sheet-vert-bottom">Weapon</div>
-			<div class="sheet-col-1-6 sheet-center sheet-small-label sheet-vert-bottom">Properties</div>
-			<div class="sheet-col-1-10 sheet-center sheet-small-label sheet-vert-bottom">Type</div>
+			<div class="sheet-col-1-18 sheet-center sheet-small-label sheet-vert-bottom">Ammo</div>
+			<div class="sheet-col-1-6 sheet-center sheet-small-label sheet-vert-bottom">Weapon</div>
+			<div class="sheet-col-1-9 sheet-center sheet-small-label sheet-vert-bottom">Type</div>
 			<div class="sheet-col-1-12 sheet-center sheet-small-label sheet-vert-bottom sheet-padr">Magic Bonus</div>
 			<div class="sheet-col-1-24 sheet-center sheet-small-label sheet-vert-bottom sheet-padr">To<br/>Hit</div>
 			<div class="sheet-col-1-12 sheet-center sheet-small-label sheet-vert-bottom">Damage Dice</div>
 			<div class="sheet-col-1-24 sheet-center sheet-small-label sheet-vert-bottom">Dmg<br/>Bonus</div>
+			<div class="sheet-col-1-8 sheet-center sheet-small-label sheet-vert-bottom sheet-padr">Damage Type</div>
 			<div class="sheet-col-1-12 sheet-center sheet-small-label sheet-vert-bottom">Crit Dmg</div>
-			<div class="sheet-col-1-10 sheet-center sheet-small-label sheet-vert-bottom">Damage Type</div>
+			<div class="sheet-col-1-12 sheet-center sheet-small-label sheet-vert-bottom sheet-padr">Crit on a</div>
+			
 			<div class="sheet-col-1-12 sheet-center sheet-small-label sheet-vert-bottom">Attack</div>
 		</div>
 		
@@ -72,10 +73,9 @@ $ranged_rows = <<<'END'
 	<!-- BEGIN ranged weapon row -->
 	<div class="sheet-row">
 		<div class="sheet-col-1-24 sheet-checkbox-row"><input type="checkbox" value="@{PB}" name="attr_pbrangedCURRENTROW" checked="checked"></div>
-		<div class="sheet-col-1-20"><input type="number" name="attr_rangedammoCURRENTROW" value="1"></div>
-		<div class="sheet-col-1-8"><input type="text" name="attr_rangedweaponnameCURRENTROW"></div>
-		<div class="sheet-col-1-6"><input type="text" name="attr_rangedweaponpropsCURRENTROW"></div>
-		<div class="sheet-col-1-10" title="Choose either normal or thrown">
+		<div class="sheet-col-1-18"><input type="number" name="attr_rangedammoCURRENTROW" value="1"></div>
+		<div class="sheet-col-1-6"><input type="text" name="attr_rangedweaponnameCURRENTROW"></div>
+		<div class="sheet-col-1-9" title="Choose either normal or thrown">
 			<select name="attr_rangedtypeCURRENTROW">
 				<option value="@{dexterity_mod}">Normal</option>
 				<option value="@{strength_mod}">Thrown</option>
@@ -86,9 +86,11 @@ $ranged_rows = <<<'END'
 		<div class="sheet-col-1-24 sheet-padr"><input type="number" name="attr_rangedtohitCURRENTROW" value="@{rangedtypeCURRENTROW} + @{pbrangedCURRENTROW} + @{rangedmagicCURRENTROW}" disabled="disabled"></div>
 		<div class="sheet-col-1-12" title="Only enter the base damage roll here without any bonuses from stats or other sources"><input class="sheet-center" type="text" name="attr_rangeddmgCURRENTROW" value="0"></div>		
 		<div class="sheet-col-1-24"><input type="number" name="attr_rangeddmgbonusCURRENTROW" value="@{rangedtypeCURRENTROW} + @{rangedmagicCURRENTROW}" disabled="disabled"></div>
+		<div class="sheet-col-1-8 sheet-padr"><input type="text" name="attr_rangeddmgtypeCURRENTROW"></div>
 		<div class="sheet-col-1-12"><input class="sheet-center" type="text" name="attr_rangedcritdmgCURRENTROW"></div>
-		<div class="sheet-col-1-10"><input type="text" name="attr_rangeddmgtypeCURRENTROW"></div>
-		<div class="sheet-col-1-12 sheet-center"><button type="roll" class="sheet-roll" name="roll_RangedAttackCURRENTROW" value="&{template:5eDefault} {{weapon=1}} {{title=@{rangedweaponnameCURRENTROW}}} {{subheader=@{character_name}}} {{subheaderright=Ranged attack}} {{subheader2=@{rangedweaponpropsCURRENTROW}}} {{attack=[[1d20 + @{rangedtohitCURRENTROW} + (@{global_ranged_attack_bonus}) ]]}} {{attackadv=[[1d20 + @{rangedtohitCURRENTROW} + (@{global_ranged_attack_bonus}) ]]}} {{damage=[[@{rangeddmgCURRENTROW} + @{rangeddmgbonusCURRENTROW} + (@{global_ranged_damage_bonus})  + 0d0]] @{rangeddmgtypeCURRENTROW}}} {{critdamage=Additional [[@{rangedcritdmgCURRENTROW}]] damage}}" >Attack</button></div>
+		<div class="sheet-col-1-12 sheet-padr"><input class="sheet-center" type="number" name="attr_rangedweaponcritrangeCURRENTROW" value="20" step="1"></div>
+	
+		<div class="sheet-col-1-12 sheet-center"><button type="roll" class="sheet-roll" name="roll_RangedAttackCURRENTROW" value="&{template:5eDefault} {{weapon=1}} {{title=@{rangedweaponnameCURRENTROW}}} {{subheader=@{character_name}}} {{subheaderright=Ranged attack}} {{attack=[[1d20cs>@{rangedweaponcritrangeCURRENTROW} + @{rangedtohitCURRENTROW} + (@{global_ranged_attack_bonus}) ]]}} {{attackadv=[[1d20cs>@{rangedweaponcritrangeCURRENTROW} + @{rangedtohitCURRENTROW} + (@{global_ranged_attack_bonus}) ]]}} {{damage=[[@{rangeddmgCURRENTROW} + @{rangeddmgbonusCURRENTROW} + (@{global_ranged_damage_bonus})  + 0d0]] @{rangeddmgtypeCURRENTROW}}} {{critdamage=Additional [[@{rangedcritdmgCURRENTROW}]] damage}}" >Attack</button></div>
 	</div>
 	<!-- END ranged weapon row -->
 
