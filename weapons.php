28db453e4e8600011038dbff923f8be8105b62c9
John Myles <jmyles@jmyles.co.uk>
2015-03-01 11:59:29 +0000

    Tweaked layout of spell row

--- weapons.php
+++ weapons.php
@@ -9,16 +9,16 @@ $melee_header = <<<'END'
 		<h4 class="sheet-center">Melee Weapons <span class="sheet-pictos">D</span></h4>
 		<div class="sheet-row sheet-sub-header">
 			<div class="sheet-col-1-24 sheet-center sheet-small-label sheet-vert-bottom">Prof?</div>
-			<div class="sheet-col-1-8 sheet-center sheet-small-label sheet-vert-bottom">Weapon</div>
-			<div class="sheet-col-1-6 sheet-center sheet-small-label sheet-vert-bottom">Properties</div>
+			<div class="sheet-col-5-24 sheet-center sheet-small-label sheet-vert-bottom">Weapon</div>
 			<div class="sheet-col-1-12 sheet-center sheet-small-label sheet-vert-bottom">Finesse?</div>
 			<div class="sheet-col-1-12 sheet-center sheet-small-label sheet-vert-bottom sheet-padr">Magic Bonus</div>
 			<div class="sheet-col-1-24 sheet-center sheet-small-label sheet-vert-bottom sheet-padr">To<br/>Hit</div>
 			<div class="sheet-col-1-12 sheet-center sheet-small-label sheet-vert-bottom">Damage Dice</div>
 			<div class="sheet-col-1-24 sheet-center sheet-small-label sheet-vert-bottom">+ stat?</div>
 			<div class="sheet-col-1-24 sheet-center sheet-small-label sheet-vert-bottom">Dmg<br/>Bonus</div>
+			<div class="sheet-col-1-8 sheet-center sheet-small-label sheet-vert-bottom sheet-padr">Damage Type</div>
 			<div class="sheet-col-1-12 sheet-center sheet-small-label sheet-vert-bottom">Crit Dmg</div>
-			<div class="sheet-col-1-8 sheet-center sheet-small-label sheet-vert-bottom">Damage Type</div>
+			<div class="sheet-col-1-12 sheet-center sheet-small-label sheet-vert-bottom sheet-padr">Crit Range</div>
 			<div class="sheet-col-1-12 sheet-center sheet-small-label sheet-vert-bottom">Attack</div>
 		</div>
 END;
@@ -27,8 +27,7 @@ $melee_rows = <<<'END'
 	<!-- BEGIN melee weapon row -->
 	<div class="sheet-row">
 		<div class="sheet-col-1-24 sheet-checkbox-row"><input type="checkbox" value="@{PB}" name="attr_pbmeleeCURRENTROW" checked="checked"></div>
-		<div class="sheet-col-1-8"><input type="text" name="attr_meleeweaponnameCURRENTROW"></div>
-		<div class="sheet-col-1-6"><input type="text" name="attr_meleeweaponpropsCURRENTROW"></div>
+		<div class="sheet-col-5-24"><input type="text" name="attr_meleeweaponnameCURRENTROW"></div>
 		<div class="sheet-col-1-12">
 			<select name="attr_meleeattackstatCURRENTROW">
 				<option value="@{strength_mod}">No</option>
@@ -40,8 +39,9 @@ $melee_rows = <<<'END'
 		<div class="sheet-col-1-12" title="Only enter the base damage roll here without any bonuses from stats or other sources"><input class="sheet-center" type="text" name="attr_meleedmgCURRENTROW" value="0"></div>
 		<div class="sheet-col-1-24 sheet-checkbox-row"><input type="checkbox" value="1" name="attr_meleeattackstatdmgCURRENTROW" checked="checked"></div>
 		<div class="sheet-col-1-24"><input type="number" name="attr_meleedmgbonusCURRENTROW" value="(@{meleeattackstatCURRENTROW} * @{meleeattackstatdmgCURRENTROW}) + @{meleemagicCURRENTROW}" disabled="disabled"></div>
+		<div class="sheet-col-1-8 sheet-padr"><input type="text" name="attr_meleedmgtypeCURRENTROW"></div>
 		<div class="sheet-col-1-12"><input class="sheet-center" type="text" name="attr_meleecritdmgCURRENTROW"></div>
-		<div class="sheet-col-1-8"><input type="text" name="attr_meleedmgtypeCURRENTROW"></div>
+		<div class="sheet-col-1-12 sheet-padr"><input type="number" name="attr_meleeweaponcritrangeCURRENTROW" value="20" step="1"></div>
 		<div class="sheet-col-1-12 sheet-center"><button type="roll" class="sheet-roll" name="roll_MeleeAttackCURRENTROW" value="&{template:5eDefault} {{weapon=1}} {{title=@{meleeweaponnameCURRENTROW}}} {{subheader=@{character_name}}} {{subheaderright=Melee attack}} {{subheader2=@{meleeweaponpropsCURRENTROW}}} {{attack=[[1d20 + @{meleetohitCURRENTROW} + (@{global_melee_attack_bonus}) ]]}} {{attackadv=[[1d20 + @{meleetohitCURRENTROW} + (@{global_melee_attack_bonus}) ]]}} {{damage=[[@{meleedmgCURRENTROW} + @{meleedmgbonusCURRENTROW} + (@{global_melee_damage_bonus})  + 0d0]] @{meleedmgtypeCURRENTROW}}} {{critdamage=Additional [[@{meleecritdmgCURRENTROW}]] damage}}" >Attack</button></div>
 	</div>
 	<!-- END melee weapon row -->
